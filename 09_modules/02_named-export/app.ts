1a976c7b69d27bef46acb105e7c480f01d3c78be
mholzmann <m.holzmann@htl-leonding.ac.at>
2022-12-01 12:04:15 +0100

    add other import methods

--- 09_modules/02_named-export/app.ts
+++ 09_modules/02_named-export/app.ts
@@ -1,9 +1,14 @@
-// the exports can be imported by using their names
-import { yellowCylinder, Cylinder, MAX_HEIGHT, cylinders, printVolume } from './cylinder-module';
+// the exports can be imported by using their names or rename them with 'as'
+import { yellowCylinder, Cylinder, MAX_HEIGHT as MAX, cylinders, printVolume } from './cylinder-module';
 
-const redCylinder: Cylinder = { radius: 2, height: MAX_HEIGHT, colour: 'red' };
+// you can import all into one object with following statement
+import * as cylinderModule from './cylinder-module';
+
+const redCylinder: Cylinder = { radius: 2, height: MAX, colour: 'red' };
 cylinders.push(redCylinder);
 
 for (const cylinder of cylinders) {
     printVolume(cylinder);
 }
+
+console.log(cylinderModule.MAX_HEIGHT);
