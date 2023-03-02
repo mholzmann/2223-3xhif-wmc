aff6199f85c6709fb9439b3534563950771b37fc
mholzmann <m.holzmann@htl-leonding.ac.at>
2023-03-02 11:22:19 +0100

    update demo

--- 11_fetch-api/03_top-level-await/pokeapi.ts
+++ 11_fetch-api/03_top-level-await/pokeapi.ts
@@ -1,8 +1,8 @@
-// top level await (no async function necessary)
-const response: Response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
-const pokemon: any = await response.json();
-console.log(pokemon);
-document.getElementById('result')!.innerHTML = `You catched a ${pokemon.name} with a height of ${pokemon.height} decimetres.`;
+const res = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+const pokemon = await res.json();
 
-// an ES6 module needs at least one import or export
-export {};
+const img = document.createElement("img");
+img.src = pokemon.sprites.front_default;
+document.getElementById("result")!.append(img);
+
+export { };
